729b8f684c00a47bc18fb86a578be531705bb78a
ark948 <armankarimi948@gmail.com>
2024-09-01 23:12:18 +0330

    Routing: router.js-RouterView-RouterLinks

---
+++ RouterVueLearning/src/router/index.js
@@ -0,0 +1,15 @@
+// DEFINE ROUTING RULES //
+
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: "/", name: "homeo", component: HomeView},
+        { path: "/about", name: "about", component: AboutView},
+    ]
+});
+
+export default router
