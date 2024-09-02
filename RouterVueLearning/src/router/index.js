9ae491528cb359b8d7a1b6999574829c2623018b
ark948 <armankarimi948@gmail.com>
2024-09-02 21:56:41 +0330

    RouterVueLearning - 404 not found page

--- RouterVueLearning/src/router/index.js
+++ RouterVueLearning/src/router/index.js
@@ -5,6 +5,7 @@ import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import CarView from "@/views/CarView.vue";
 import ContactView from "@/views/ContactView.vue";
+import NotFound from "@/components/NotFound.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,10 @@ const router = createRouter({
         { path: "/about", name: "about", component: AboutView },
         { path: "/cars/:id", name: "car", component: CarView, children: [
             { path: "contact", component: ContactView } // nested route
-        ] }
+        ] },
+
+        // using regex to catch all routes not listed above (404)
+        { path: "/:catchall(.*)*", name: "Not Found", component: NotFound }
     ]
 });
 
