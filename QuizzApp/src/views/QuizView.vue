d082e65e4f92a86ee8d9f41045084af27e9e957f
ark948 <armankarimi948@gmail.com>
2024-09-03 15:01:20 +0330

    QuizzApp - more stuff

--- QuizzApp/src/views/QuizView.vue
+++ QuizzApp/src/views/QuizView.vue
@@ -1,13 +1,22 @@
 <script setup>
     import Question from '@/components/Question.vue';
     import QuizHeader from '@/components/QuizHeader.vue';
+    import { ref } from 'vue';
+    import { useRoute } from 'vue-router';
+    import quizes from '../data/quizes.json'
+
+    const route = useRoute()
+    const quizId = parseInt(route.params.id)
+    // we need to keep track of the current question, starting from 1
+    const currentQuestionIndex = ref(0);
+    const quiz = quizes.find(q => q.id === quizId)
 </script>
 
 <template>
     <div>
         <QuizHeader />
         <div>
-            <Question />
+            <Question :question="quiz.questions[currentQuestionIndex]" />
         </div>
     </div>
 </template>
