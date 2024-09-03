c173d70ee2dad4b84b8ac3a543848fe6963449c1
ark948 <armankarimi948@gmail.com>
2024-09-03 15:24:45 +0330

    QuizApp - dynamic progress bar precentage

--- QuizzApp/src/views/QuizView.vue
+++ QuizzApp/src/views/QuizView.vue
@@ -1,7 +1,7 @@
 <script setup>
     import Question from '@/components/Question.vue';
     import QuizHeader from '@/components/QuizHeader.vue';
-    import { ref, watch } from 'vue';
+    import { ref, watch, computed } from 'vue';
     import { useRoute } from 'vue-router';
     import quizes from '../data/quizes.json'
 
@@ -10,18 +10,16 @@
     // we need to keep track of the current question, starting from 1
     const currentQuestionIndex = ref(0);
     const quiz = quizes.find(q => q.id === quizId)
-
-    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
-
-    // listen to currentQuestionIndex, and upon change of its value, run the callback
-    watch(() => currentQuestionIndex.value, () => {
-        questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
-    })
+    const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+    const barPercentage = computed(() => `${currentQuestionIndex.value/quiz.questions.length * 100}%`)
 </script>
 
 <template>
     <div>
-        <QuizHeader :questionStatus="questionStatus" />
+        <QuizHeader 
+        :questionStatus="questionStatus"
+        :barPercentage="barPercentage"
+        />
         <div>
             <Question :question="quiz.questions[currentQuestionIndex]" />
         </div>
