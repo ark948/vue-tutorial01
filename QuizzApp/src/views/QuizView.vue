afbebc518c3dcd7de7476257e74356060d415f04
ark948 <armankarimi948@gmail.com>
2024-09-03 15:13:09 +0330

    QuizzApp: pasing properties - more advanced

--- QuizzApp/src/views/QuizView.vue
+++ QuizzApp/src/views/QuizView.vue
@@ -1,7 +1,7 @@
 <script setup>
     import Question from '@/components/Question.vue';
     import QuizHeader from '@/components/QuizHeader.vue';
-    import { ref } from 'vue';
+    import { ref, watch } from 'vue';
     import { useRoute } from 'vue-router';
     import quizes from '../data/quizes.json'
 
@@ -10,13 +10,21 @@
     // we need to keep track of the current question, starting from 1
     const currentQuestionIndex = ref(0);
     const quiz = quizes.find(q => q.id === quizId)
+
+    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+    // listen to currentQuestionIndex, and upon change of its value, run the callback
+    watch(() => currentQuestionIndex.value, () => {
+        questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    })
 </script>
 
 <template>
     <div>
-        <QuizHeader />
+        <QuizHeader :questionStatus="questionStatus" />
         <div>
             <Question :question="quiz.questions[currentQuestionIndex]" />
         </div>
+        <button @click="currentQuestionIndex++">Next Question</button>
     </div>
 </template>
